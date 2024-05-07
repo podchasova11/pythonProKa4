6b1cc9e8bb4c3e12c369b0912458c408468e1ea3
podchasova11 <Pod4asova20@gmail.com>
2024-05-07 12:12:47 +0300

    added ls10

--- hw_10.py
+++ hw_10.py
@@ -1,6 +1,6 @@
 import time
 
-from  selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.options import Options
 
 options = Options()
@@ -8,4 +8,15 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("--user-agent=XSDTRCYFGUVJYBHIKULNM")
 driver = webdriver.Chrome(options=options)
 
+
+driver.get("https://hyperskill.org/tracks")
+FOR_BUSINES_BUTTON_LOCATOR = (("xpath"), "//a[text()=' For Business '])[1]]")
+START_FOR_FREE_BUTTON_LOCATOR = (("xpath"), "//a[text()='Start for Free'])[1]]")
+driver.find_element(*FOR_BUSINES_BUTTON_LOCATOR).click()
+time.sleep(3)
+
+tabs = driver.window_handles
+driver.switch_to.window(tabs[1])
+driver.find_element(*START_FOR_FREE_BUTTON_LOCATOR).click()
+time.sleep(3)
 
